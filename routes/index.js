6a4d99f679036a697bdcbff11932f59d8e84b1d4
takayuki.fujisawa <corda.tigre.net@gmail.com>
2015-12-08 01:02:01 +0900

    超簡易版のlogin/me/logout #6

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,64 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/login', function(req, res, next) {
+  req.session.user = {
+    name: req.body.name,
+    email: req.body.email,
+    groupe: req.body.group,
+  };
+
+  var response =   {
+    "user": {
+        "name": req.session.user.name,
+        "email": req.session.user.email,
+    },
+    "_links": {
+        "self" : { "method": "GET", "href": "/me" },
+        "calendar" : { "method": "GET", "href": "/group/group12345/calender" },
+        "logout" : { "method": "GET", "href": "/logout" },
+    },
+    "_embeded": "",
+  }
+
+  res.send(response);
+});
+
+router.get('/me', function(req, res, next) {
+
+  var response = "";
+  if (req.session.user) {
+    response =   {
+      "user": {
+          "name": req.session.user.name,
+          "email": req.session.user.email,
+      },
+      "_links": {
+          "self" : { "method": "GET", "href": "/me" },
+      },
+      "_embeded": "",
+    }
+  } else {
+    response =   {
+      "_links": {
+          "self" : { "method": "GET", "href": "/me" },
+      },
+      "_embeded": "",
+    }
+  }
+
+  res.send(response);
+});
+
+router.get('/logout', function(req, res, next) {
+  req.session.destroy();
+
+  var response =   {
+    "_links": "",
+    "_embeded": "",
+  }
+
+  res.send(response);
+});
+
 module.exports = router;
