8fff6c025dc8d6f4f295b7c491cadb7fe60531af
kawanamiyuu <i.am.really.you@gmail.com>
2015-12-11 15:29:23 +0900

    fix eslint violations of index.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,15 @@ var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 
 gulp.task('eslint', function () {
-	return gulp.src(['**/*.js', '!node_modules/**'])
+	return gulp.src(['**/*.js', '!gulpfile.js', '!node_modules/**'])
 		.pipe(eslint({
 			extends: 'xo',
 			envs: [
 				'node'
-			]
+			],
+			rules: {
+				'new-cap': [2, {'capIsNewExceptions': ['express.Router']}]
+			}
 		}))
 		.pipe(eslint.format())
 		.pipe(eslint.failOnError());
