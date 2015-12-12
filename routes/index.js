cf035cc552c3ac1265c66cd4a9a49875ee0cde0d
takayuki.fujisawa <corda.tigre.net@gmail.com>
2015-12-12 15:33:05 +0900

    impl /me (prototype) #6

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,7 @@ router.get('/', function (req, res) {
 
 router.post('/login', function (req, res) {
 	// <TODO> accepting request and validation
+	// <TODO> 上位処理に移行予定
 
 	// generate token
 	var userToken = randtoken.uid(24);
@@ -37,29 +38,84 @@ router.post('/login', function (req, res) {
 		if (result) {
 			console.log('add new user [' + result.insertedIds + ']');
 		}
+
+		// create response body
+		var response = {
+			v: API_VERSION,
+			token: userToken,
+			user: newUser,
+			_links: {
+				self: {
+					method: 'GET',
+					href: '/me',
+					headers: {
+						'Content-Type': 'application/json',
+						'X-Meshido-ApiVerion': API_VERSION,
+						'X-Meshido-UserToken': newUser.token
+					},
+					parameters: ''
+				}
+			},
+			_embeded: ''
+		};
+
+		res.send(response);
 	});
+});
 
-	// create response body
-	var response = {
-		v: API_VERSION,
-		token: userToken,
-		user: newUser,
-		_links: {
-			self: {
-				method: 'GET',
-				href: '/me',
-				headers: {
-					'Content-Type': 'application/json',
-					'X-Meshido-ApiVerion': API_VERSION,
-					'X-Meshido-UsrToken': newUser.token
-				},
-				parameters: ''
+router.get('/me', function (req, res) {
+	// accepting request and validation
+	// <TODO> 上位処理に移行予定
+	var xToken = req.get('X-Meshido-UserToken');
+	if (xToken === undefined) {
+		var errBody = {error: 'no token was send.'};
+		res.status(400).send(errBody);
+		return;
+	}
+
+	// find an user by token in request header
+	db.user.findOne({token: xToken},
+		function (err, result) {
+			if (err) {
+				console.log('faild to retrieve user');
+				throw err;
 			}
-		},
-		_embeded: ''
-	};
 
-	res.send(response);
+			// no result
+			if (result === null) {
+				var errBody = {error: 'user does not exist.'};
+				res.status(401).send(errBody);
+				return;
+			}
+
+			// exists.
+			var me = {
+				name: result.name,
+				email: result.email
+			};
+
+			// create response body
+			var response = {
+				v: API_VERSION,
+				user: me,
+				_links: {
+					self: {
+						method: 'GET',
+						href: '/me',
+						headers: {
+							'Content-Type': 'application/json',
+							'X-Meshido-ApiVerion': API_VERSION,
+							'X-Meshido-UserToken': xToken
+						},
+						parameters: ''
+					}
+				},
+				_embeded: ''
+			};
+
+			res.send(response);
+		}
+	);
 });
 
 module.exports = router;
