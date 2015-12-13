2f82f495f0d3e3e029bb9d4cb69964fe9054b409
takayuki.fujisawa <corda.tigre.net@gmail.com>
2015-12-13 21:44:23 +0900

    configulation module #14

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var config = require('config');
 
 /* GET home page. */
 router.get('/', function (req, res) {
 	res.render('index', {
-		title: 'Express'
+		title: 'Express(' + config.app.info + ')'
 	});
 });
 
