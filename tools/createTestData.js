6f2d07694ddf96fe39c193bd990385254ca29ac4
takayuki.fujisawa <corda.tigre.net@gmail.com>
2015-12-20 22:46:45 +0900

    lint 激おこ #24

--- tools/createTestData.js
+++ tools/createTestData.js
@@ -12,112 +12,111 @@ var bluebird = require('bluebird');
 bluebird.promisifyAll(mongoskin);
 
 var testUsers = [
-	{ name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com", group : "test1", token : "testtoken001" },
-	{ name : "テスト太郎2", email : "corda.tigre.net+101@gmail.com", group : "test1", token : "testtoken002" },
-	{ name : "テスト太郎3", email : "corda.tigre.net+102@gmail.com", group : "test1", token : "testtoken003" }
+	{name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com', group: 'test1', token: 'testtoken001'},
+	{name: 'テスト太郎2', email: 'corda.tigre.net+101@gmail.com', group: 'test1', token: 'testtoken002'},
+	{name: 'テスト太郎3', email: 'corda.tigre.net+102@gmail.com', group: 'test1', token: 'testtoken003'}
 ];
 
 var testEvents = [
-	{ group : "test1", "year" : 2015, "month" : 11, "day" : 10, "type" : "dinner",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 11, "day" : 10, "type" : "lunch",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 11, "day" : 15, "type" : "dinner",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 11, "day" : 15, "type" : "lunch",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 11, "day" : 20, "type" : "dinner",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 11, "day" : 20, "type" : "lunch",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
+	{group: 'test1', year: 2015, month: 11, day: 10, type: 'dinner',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
+	{group: 'test1', year: 2015, month: 11, day: 10, type: 'lunch',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
+	{group: 'test1', year: 2015, month: 11, day: 15, type: 'dinner',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
+	{group: 'test1', year: 2015, month: 11, day: 15, type: 'lunch',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
+	{group: 'test1', year: 2015, month: 11, day: 20, type: 'dinner',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
+	{group: 'test1', year: 2015, month: 11, day: 20, type: 'lunch',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
 
-	{ group : "test1", "year" : 2015, "month" : 11, "day" : 10, "type" : "dinner",
-		"user" : { name : "テスト太郎2", email : "corda.tigre.net+101@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 11, "day" : 16, "type" : "dinner",
-		"user" : { name : "テスト太郎2", email : "corda.tigre.net+101@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 11, "day" : 21, "type" : "dinner",
-		"user" : { name : "テスト太郎2", email : "corda.tigre.net+101@gmail.com" }},
+	{group: 'test1', year: 2015, month: 11, day: 10, type: 'dinner',
+		user: {name: 'テスト太郎2', email: 'corda.tigre.net+101@gmail.com'}},
+	{group: 'test1', year: 2015, month: 11, day: 16, type: 'dinner',
+		user: {name: 'テスト太郎2', email: 'corda.tigre.net+101@gmail.com'}},
+	{group: 'test1', year: 2015, month: 11, day: 21, type: 'dinner',
+		user: {name: 'テスト太郎2', email: 'corda.tigre.net+101@gmail.com'}},
 
-	{ group : "test1", "year" : 2015, "month" : 11, "day" : 10, "type" : "lunch",
-		"user" : { name : "テスト太郎3", email : "corda.tigre.net+102@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 11, "day" : 14, "type" : "lunch",
-		"user" : { name : "テスト太郎3", email : "corda.tigre.net+102@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 11, "day" : 19, "type" : "lunch",
-		"user" : { name : "テスト太郎3", email : "corda.tigre.net+102@gmail.com" }},
-/////////////
-	{ group : "test1", "year" : 2015, "month" : 12, "day" : 5, "type" : "dinner",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 12, "day" : 5, "type" : "lunch",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 12, "day" : 10, "type" : "dinner",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 12, "day" : 10, "type" : "lunch",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 12, "day" : 15, "type" : "dinner",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 12, "day" : 15, "type" : "lunch",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
+	{group: 'test1', year: 2015, month: 11, day: 10, type: 'lunch',
+		user: {name: 'テスト太郎3', email: 'corda.tigre.net+102@gmail.com'}},
+	{group: 'test1', year: 2015, month: 11, day: 14, type: 'lunch',
+		user: {name: 'テスト太郎3', email: 'corda.tigre.net+102@gmail.com'}},
+	{group: 'test1', year: 2015, month: 11, day: 19, type: 'lunch',
+		user: {name: 'テスト太郎3', email: 'corda.tigre.net+102@gmail.com'}},
 
-	{ group : "test1", "year" : 2015, "month" : 12, "day" : 5, "type" : "dinner",
-		"user" : { name : "テスト太郎2", email : "corda.tigre.net+101@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 12, "day" : 11, "type" : "dinner",
-		"user" : { name : "テスト太郎2", email : "corda.tigre.net+101@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 12, "day" : 16, "type" : "dinner",
-		"user" : { name : "テスト太郎2", email : "corda.tigre.net+101@gmail.com" }},
+	{group: 'test1', year: 2015, month: 12, day: 5, type: 'dinner',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
+	{group: 'test1', year: 2015, month: 12, day: 5, type: 'lunch',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
+	{group: 'test1', year: 2015, month: 12, day: 10, type: 'dinner',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
+	{group: 'test1', year: 2015, month: 12, day: 10, type: 'lunch',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
+	{group: 'test1', year: 2015, month: 12, day: 15, type: 'dinner',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
+	{group: 'test1', year: 2015, month: 12, day: 15, type: 'lunch',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
 
-	{ group : "test1", "year" : 2015, "month" : 12, "day" : 5, "type" : "lunch",
-		"user" : { name : "テスト太郎3", email : "corda.tigre.net+102@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 12, "day" : 9, "type" : "lunch",
-		"user" : { name : "テスト太郎3", email : "corda.tigre.net+102@gmail.com" }},
-	{ group : "test1", "year" : 2015, "month" : 12, "day" : 14, "type" : "lunch",
-		"user" : { name : "テスト太郎3", email : "corda.tigre.net+102@gmail.com" }},
+	{group: 'test1', year: 2015, month: 12, day: 5, type: 'dinner',
+		user: {name: 'テスト太郎2', email: 'corda.tigre.net+101@gmail.com'}},
+	{group: 'test1', year: 2015, month: 12, day: 11, type: 'dinner',
+		user: {name: 'テスト太郎2', email: 'corda.tigre.net+101@gmail.com'}},
+	{group: 'test1', year: 2015, month: 12, day: 16, type: 'dinner',
+		user: {name: 'テスト太郎2', email: 'corda.tigre.net+101@gmail.com'}},
 
-/////////////
-	{ group : "test1", "year" : 2016, "month" : 1, "day" : 1, "type" : "dinner",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
-	{ group : "test1", "year" : 2016, "month" : 1, "day" : 1, "type" : "lunch",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
-	{ group : "test1", "year" : 2016, "month" : 1, "day" : 11, "type" : "dinner",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
-	{ group : "test1", "year" : 2016, "month" : 1, "day" : 11, "type" : "lunch",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
-	{ group : "test1", "year" : 2016, "month" : 1, "day" : 21, "type" : "dinner",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
-	{ group : "test1", "year" : 2016, "month" : 1, "day" : 21, "type" : "lunch",
-		"user" : { name : "テスト太郎1", email : "corda.tigre.net+100@gmail.com" }},
+	{group: 'test1', year: 2015, month: 12, day: 5, type: 'lunch',
+		user: {name: 'テスト太郎3', email: 'corda.tigre.net+102@gmail.com'}},
+	{group: 'test1', year: 2015, month: 12, day: 9, type: 'lunch',
+		user: {name: 'テスト太郎3', email: 'corda.tigre.net+102@gmail.com'}},
+	{group: 'test1', year: 2015, month: 12, day: 14, type: 'lunch',
+		user: {name: 'テスト太郎3', email: 'corda.tigre.net+102@gmail.com'}},
 
-	{ group : "test1", "year" : 2016, "month" : 1, "day" : 1, "type" : "dinner",
-		"user" : { name : "テスト太郎2", email : "corda.tigre.net+101@gmail.com" }},
-	{ group : "test1", "year" : 2016, "month" : 1, "day" : 12, "type" : "dinner",
-		"user" : { name : "テスト太郎2", email : "corda.tigre.net+101@gmail.com" }},
-	{ group : "test1", "year" : 2016, "month" : 1, "day" : 22, "type" : "dinner",
-		"user" : { name : "テスト太郎2", email : "corda.tigre.net+101@gmail.com" }},
+	{group: 'test1', year: 2016, month: 1, day: 1, type: 'dinner',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
+	{group: 'test1', year: 2016, month: 1, day: 1, type: 'lunch',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
+	{group: 'test1', year: 2016, month: 1, day: 11, type: 'dinner',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
+	{group: 'test1', year: 2016, month: 1, day: 11, type: 'lunch',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
+	{group: 'test1', year: 2016, month: 1, day: 21, type: 'dinner',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
+	{group: 'test1', year: 2016, month: 1, day: 21, type: 'lunch',
+		user: {name: 'テスト太郎1', email: 'corda.tigre.net+100@gmail.com'}},
 
-	{ group : "test1", "year" : 2016, "month" : 1, "day" : 1, "type" : "lunch",
-		"user" : { name : "テスト太郎3", email : "corda.tigre.net+102@gmail.com" }},
-	{ group : "test1", "year" : 2016, "month" : 1, "day" : 10, "type" : "lunch",
-		"user" : { name : "テスト太郎3", email : "corda.tigre.net+102@gmail.com" }},
-	{ group : "test1", "year" : 2016, "month" : 1, "day" : 20, "type" : "lunch",
-		"user" : { name : "テスト太郎3", email : "corda.tigre.net+102@gmail.com" }}
+	{group: 'test1', year: 2016, month: 1, day: 1, type: 'dinner',
+		user: {name: 'テスト太郎2', email: 'corda.tigre.net+101@gmail.com'}},
+	{group: 'test1', year: 2016, month: 1, day: 12, type: 'dinner',
+		user: {name: 'テスト太郎2', email: 'corda.tigre.net+101@gmail.com'}},
+	{group: 'test1', year: 2016, month: 1, day: 22, type: 'dinner',
+		user: {name: 'テスト太郎2', email: 'corda.tigre.net+101@gmail.com'}},
+
+	{group: 'test1', year: 2016, month: 1, day: 1, type: 'lunch',
+		user: {name: 'テスト太郎3', email: 'corda.tigre.net+102@gmail.com'}},
+	{group: 'test1', year: 2016, month: 1, day: 10, type: 'lunch',
+		user: {name: 'テスト太郎3', email: 'corda.tigre.net+102@gmail.com'}},
+	{group: 'test1', year: 2016, month: 1, day: 20, type: 'lunch',
+		user: {name: 'テスト太郎3', email: 'corda.tigre.net+102@gmail.com'}}
 ];
 
 var promisses = [];
 
-for (var i=0; i<testUsers.length; i++) {
+for (var i = 0; i < testUsers.length; i++) {
 	promisses.push(
 		db.collection('users').insertAsync(testUsers[i])
 	);
 }
 
-for (var i=0; i<testEvents.length; i++) {
+for (var j = 0; j < testEvents.length; j++) {
 	promisses.push(
-		db.collection('events').insertAsync(testEvents[i])
+		db.collection('events').insertAsync(testEvents[j])
 	);
 }
 
 console.log('insert test data');
 Promise.all(promisses)
-.then(function(){
+.then(function () {
 	console.log('finish insert test data');
 	process.exit(0);
 })
